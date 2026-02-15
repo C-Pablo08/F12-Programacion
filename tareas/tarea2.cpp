d751e5f80a10e4e936a7759b25768e9c460834cc
C-Pablo08 <carlosdavidpablohernandez@gmail.com>
2026-02-14 23:10:02 -0600

    moví tarea2 a carpeta tareas

---
+++ tareas/tarea2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main() {
+  std::cout << "Hola C++ desde code-server\n";
+  std::cout << "<<Nombre: Carlos David Pablo Hernández>> y <<Carnet: 202604171>>\n";
+  return 0;
+}
