e8cb90a1243bc837619dbc2a60bea347db1283f0
C-Pablo08 <carlosdavidpablohernandez@gmail.com>
2026-02-14 23:08:37 -0600

    restauré el ejemplo y moví las modificaciones

--- cpp/ejemplos/hola_mundo.cpp
+++ cpp/ejemplos/hola_mundo.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main() {
   std::cout << "Hola C++ desde code-server\n";
-  std::cout << "<<Nombre: Carlos David Pablo Hernández>> y <<Carnet: 202604171>>\n";
+  std::cout << "<<Nombre>> y <<Carnet>>\n";
   return 0;
 }
